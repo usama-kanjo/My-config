ee19ff050599689feafa8133ea663cafa6e77058
M osama kanjo <m.osama.kanjo2007@gmail.com>
2026-05-31 22:24:28 +0300

    fix: hide Kitty cursor trail when Neovim opens via smear-cursor.nvim

--- dot_config/nvim/lua/plugins/smear_cursor.lua
+++ dot_config/nvim/lua/plugins/smear_cursor.lua
@@ -1,4 +1,20 @@
 return {
   "sphamba/smear-cursor.nvim",
   opts = {},
+  init = function()
+    local bg = "#1a1a1a"
+    local cursor_color = "#3F2254"
+
+    vim.api.nvim_create_autocmd("VimEnter", {
+      once = true,
+      callback = function()
+        pcall(vim.fn.system, { "kitty", "@", "set-colors", "cursor_trail_color=" .. bg })
+      end,
+    })
+    vim.api.nvim_create_autocmd("VimLeave", {
+      callback = function()
+        pcall(vim.fn.system, { "kitty", "@", "set-colors", "cursor_trail_color=" .. cursor_color })
+      end,
+    })
+  end,
 }
